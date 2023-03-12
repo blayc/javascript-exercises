beddfa2d02075f7e797b6d52796ccc490426c157
Blake Jacobs <blake.jacobs07@gmail.com>
2023-03-12 12:38:49 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  convertedTemp = (temp - 32) * 5/9; 
+  return +convertedTemp.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  convertedTemp = temp * 1.8 + 32; 
+  return +convertedTemp.toFixed(1)
 };
 
 // Do not edit below this line
