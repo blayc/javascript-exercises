2f5005db992b972cc3dc1cabb3070a7fb72ac3c1
Blake Jacobs <blake.jacobs07@gmail.com>
2023-03-11 20:23:26 -0600

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strChars = str.split("");
+    let reversedStr = strChars.reverse().join("");
+    return reversedStr; 
 };
 
 // Do not edit below this line
