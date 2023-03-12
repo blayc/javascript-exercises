745a83560c63181fd518e99945ab3fa6e1170db3
Blake Jacobs <blake.jacobs07@gmail.com>
2023-03-11 21:33:01 -0600

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function (array, ...elements) 
+    {
+        for (i = 0; i < elements.length; i++)
+            {
+                const element = elements[i]; 
+                if (array.includes(element)) 
+                    {array.splice(array.indexOf(element), 1)}
+            }
+        return array;
+    }
 // Do not edit below this line
 module.exports = removeFromArray;
