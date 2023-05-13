f3733d9ac88d79570e5056230bf07a93f7e013b2
Blake Jacobs <blake.jacobs07@gmail.com>
2023-05-13 14:34:02 -0500

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, number) => {
+    return total + number
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, number) => {
+    return product * number
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1; 
+  for(i = 1; i <= n; i++) {
+    result *= i
+  };
+  return result;
 };
 
 // Do not edit below this line
