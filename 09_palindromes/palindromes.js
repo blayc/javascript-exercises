8ca48b25d8c021c9f8562de83c9390590980b228
Blake Jacobs <blake.jacobs07@gmail.com>
2023-05-13 15:20:19 -0500

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const letters = string
+        .toLowerCase()
+        .split('')
+        .filter(character => /[a-z]/.test(character))
+        ; 
+    const reverseLetters = [...letters].reverse(); 
+    return letters.join('') === reverseLetters.join('')
 };
 
 // Do not edit below this line
