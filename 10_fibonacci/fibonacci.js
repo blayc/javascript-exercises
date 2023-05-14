4ddc0114744acaa30c29fb972c8398f5e2b9df4c
Blake Jacobs <blake.jacobs07@gmail.com>
2023-05-13 22:32:01 -0500

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let fibSequence = [1,1]; 
+    if(n < 0){return 'OOPS'}
+    else {
+    for(i = 2; i <= n; i++) {
+        let a = fibSequence[i - 2]; 
+        let b = fibSequence[i - 1]; 
+        let c = a + b; 
+        fibSequence.push(c);
+    }
+}
+    return (fibSequence[n - 1])
 };
 
 // Do not edit below this line
